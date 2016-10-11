3feb40f246f08730053a5b3c5203271847130bfb
Raju Gurumurthy <rgurumur@akamai.com>
2016-10-11 19:48:12 +0530

    Pushing to GIT

---
+++ Xchange.com.node/public/javascripts/angularApp.js
@@ -0,0 +1,285 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+/*angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+*/
+
+
+angular.module('myApp',['ui.router'] )
+    /*.factory('auth', ['$http', '$window', function($http, $window){
+        var auth = {};
+
+        auth.saveToken = function (token){
+            $window.localStorage['flapper-news-token'] = token;
+        };
+
+        auth.getToken = function (){
+            return $window.localStorage['flapper-news-token'];
+        };
+
+        auth.isLoggedIn = function(){
+            var token = auth.getToken();
+
+            if(token){
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        auth.currentUser = function(){
+            if(auth.isLoggedIn()){
+                var token = auth.getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.username;
+            }
+        };
+
+        auth.register = function(user){
+            return $http.post('/register', user).success(function(data){
+                auth.saveToken(data.token);
+            });
+        };
+
+        auth.logIn = function(user){
+            return $http.post('/login', user).success(function(data){
+                auth.saveToken(data.token);
+            });
+        };
+
+        auth.logOut = function(){
+            $window.localStorage.removeItem('flapper-news-token');
+        };
+
+        return auth;
+    }]) */
+    .factory('posts', ['$http',  function($http){
+  var o = {
+      posts: []
+  };
+
+
+    o.getAll = function() {
+        return $http.get('/posts').success(function(data){
+            angular.copy(data, o.posts);
+        });
+    };
+
+    o.getAllByISBN = function(findISBN) {
+        return $http({
+            url: '/search/byISBN',
+            method: "GET",
+            params: {findISBN: findISBN}
+        }).success(function(data){
+            angular.copy(data, o.posts);
+        });
+    };
+
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data){
+            o.posts.push(data);
+        });
+    };
+
+    o.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote')
+            .success(function(data){
+                post.upvotes += 1;
+            });
+    };
+
+    o.get = function(id) {
+        return $http.get('/posts/' + id).then(function(res){
+            return res.data;
+        });
+    };
+
+    o.addComment = function(id, comment) {
+        return $http.post('/posts/' + id + '/comments', comment);
+    };
+
+    o.upvoteComment = function(post, comment) {
+        return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+            .success(function(data){
+                comment.upvotes += 1;
+            });
+    };
+        return o;
+
+
+    }])
+.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function($scope,posts){
+    $scope.test = 'Hello world!';
+    $scope.posts = [];
+
+      $scope.addPost = function(){
+          if(!$scope.title || $scope.title === '') { return; }
+          posts.create({
+              title: $scope.title,
+              description: $scope.description,
+              bookAuthor: $scope.bookAuthor,
+              ISBN: $scope.ISBN,
+              link: $scope.link
+              
+          });
+          $scope.title = '';
+          $scope.description = '';
+          $scope.bookAuthor = '';
+          $scope.ISBN = '';
+          $scope.link = '';
+
+      };
+
+      $scope.incrementUpvotes = function(post) {
+          posts.upvote(post);
+      };
+
+
+      $scope.posts = posts.posts;
+
+      console.log('scope' +$scope.posts);
+
+ //     $scope.isLoggedIn = auth.isLoggedIn;
+  }])
+
+.controller('PostsCtrl', [
+    '$scope',
+    'posts',
+    'post',
+    function($scope, posts, post){
+        $scope.post = post;
+
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+            posts.addComment(post._id, {
+                body: $scope.body,
+                author: 'user',
+            }).success(function(comment) {
+                $scope.post.comments.push(comment);
+            });
+            $scope.body = '';
+        };
+
+        $scope.incrementUpvotes = function(comment){
+            posts.upvoteComment(post, comment);
+        };
+
+ //       $scope.isLoggedIn = auth.isLoggedIn;
+
+    }])
+/*    .controller('AuthCtrl', [
+        '$scope',
+        '$state',
+        'auth',
+        function($scope, $state, auth){
+            $scope.user = {};
+
+            $scope.register = function(){
+                auth.register($scope.user).error(function(error){
+                    $scope.error = error;
+                }).then(function(){
+                    $state.go('home');
+                });
+            };
+
+            $scope.logIn = function(){
+                auth.logIn($scope.user).error(function(error){
+                    $scope.error = error;
+                }).then(function(){
+                    $state.go('home');
+                });
+            };
+        }])
+    .controller('NavCtrl', [
+        '$scope',
+        'auth',
+        function($scope, auth){
+            $scope.isLoggedIn = auth.isLoggedIn;
+            $scope.currentUser = auth.currentUser;
+            $scope.logOut = auth.logOut;
+        }])*/
+    .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['posts', function(posts){
+                        console.log('state'+posts.getAll())
+                        return posts.getAll();
+                    }]
+                }
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl',
+                resolve: {
+                    post: ['$stateParams', 'posts', function($stateParams, posts) {
+                        return posts.get($stateParams.id);
+                    }]
+                }
+            })
+            .state('search', {
+                url: '/search/byISBN?findISBN',
+                templateUrl: '/search.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['$stateParams','posts', function($stateParams ,posts){
+                        return posts.getAllByISBN($stateParams.findISBN);
+                    }]
+                }
+            })
+/*
+            .state('login', {
+                url: '/login',
+                templateUrl: '/login.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth){
+                    if(auth.isLoggedIn()){
+                        $state.go('home');
+                    }
+                }]
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/register.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth){
+                    if(auth.isLoggedIn()){
+                        $state.go('home');
+                    }
+                }]
+            });
+*/
+
+        $urlRouterProvider.otherwise('home');
+    }]);
+
+
+
+
+
+
+
